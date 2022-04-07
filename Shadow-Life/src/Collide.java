801bce50bbae72c4a9401e989e161fed465fbcb4
Jerry2108 <80389972+Jerry2108@users.noreply.github.com>
2022-04-07 15:32:11 +1000

    Delete Shadow-Life directory

--- Shadow-Life/src/Collide.java
+++
@@ -1,7 +0,0 @@
-public interface Collide{
-    abstract void collideStockPile(Actor stockPile);
-    abstract void collideHoard(Actor hoard);
-    abstract void collideTree(Actor tree);
-    abstract void collidePad();
-    abstract void collideFence();
-}
